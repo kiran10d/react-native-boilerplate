53f9bbf74448dbeb725e63babae53dc7ef258a16
Kiran Kumar <kirankumar@10decoders.in>
2022-08-29 22:06:13 +0530

    Added Global Styles

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+export const GlobalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  btnPrimary: {
+    backgroundColor: '#236BFE',
+    color: '#fff',
+  },
+  btnSecondary: {
+    backgroundColor: '#F1F0F5',
+  },
+});
